63c05a9c68c9f9086de7a1e90f8d4315a0fd1c33
thisiskicker <thisiskicker@gmail.com>
2023-07-27 14:22:24 -0500

    terraforge

---
+++ cluster/main.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+  location = var.location
+}
+resource "azurerm_kubernetes_cluster" "aks" {
+  name = var.cluster_name
+  kubernetes_version = var.kubernetes_version
+  location = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix = var.cluster_name
+  node_resource_group = var.node_resource_group
+  default_node_pool {
+    name = "system"
+    node_count = var.system_node_count
+    vm_size = "standard_A2_v2"
+    type = "VirtualMachineScaleSets"
+    #availability_zones = [1, 2, 3]
+    enable_auto_scaling = false
+    vnet_subnet_id        = azurerm_subnet.aks-default.id
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+  network_profile {
+    load_balancer_sku = "standard"
+    network_plugin = "azure"
+  }
+}
---
+++ cluster/network.tf
@@ -0,0 +1,15 @@
+# Create Virtual Network
+resource "azurerm_virtual_network" "aksvnet" {
+  name                = "aks-network"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/8"]
+}
+
+# Create a Subnet for AKS
+resource "azurerm_subnet" "aks-default" {
+  name                 = "aks-default-subnet"
+  virtual_network_name = azurerm_virtual_network.aksvnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefixes     = ["10.240.0.0/16"]
+}
