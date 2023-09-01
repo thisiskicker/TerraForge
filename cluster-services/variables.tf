888a2b20fdac2074068aee28596e5617b778a995
thisiskicker <thisiskicker@gmail.com>
2023-09-01 13:33:38 -0500

    disable variables

--- cluster-services/variables.tf
+++ cluster-services/variables.tf
@@ -1,13 +1,13 @@
-resource "local_sensitive_file" "kconfig_file" {
-    content = var.kconfig
-    filename = "${path.module}/kconfig"
-}
+# resource "local_sensitive_file" "kconfig_file" {
+#     content = var.kconfig
+#     filename = "${path.module}/kconfig"
+# }
 
-variable "kconfig" {
-    type = string
-    default = ""
-    sensitive = true
-}
+# variable "kconfig" {
+#     type = string
+#     default = ""
+#     sensitive = true
+# }
 
 # variable "kcontext" {
 #     type = string
