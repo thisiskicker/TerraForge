aa1aa2992d3ddaca7bfd550db1ec122725e288e1
thisiskicker <thisiskicker@gmail.com>
2023-09-01 12:17:08 -0500

    depends on

--- cluster-services/providers.tf
+++ cluster-services/providers.tf
@@ -16,9 +16,11 @@ provider "helm" {
   kubernetes {
     config_path = "${path.module}/kconfig"
   }
+  depends_on = local_sensitive_file.kconfig_file
 }
 
 provider "kubectl" {
   config_path = "${path.module}/kconfig"
   # config_context = var.kcontext
+    depends_on = local_sensitive_file.kconfig_file
 }
