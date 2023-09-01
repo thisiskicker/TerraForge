3c098b923b18fbf7c99eac653e3cc6a82a6a044b
thisiskicker <thisiskicker@gmail.com>
2023-09-01 10:38:59 -0500

    declare variable

--- cluster-services/variables.tf
+++ cluster-services/variables.tf
@@ -1,3 +1,8 @@
+variable "kconfig" {
+    type = string
+    default = ""
+}
+
 resource "local_sensitive_file" "kconfig" {
     content = terraform.workspace.var.kconfig
     filename = "${path.module}/kconfig"
