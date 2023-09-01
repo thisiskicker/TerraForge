e8651f95dd74bb15fd5333bdf6ae1bbc24c14254
thisiskicker <thisiskicker@gmail.com>
2023-09-01 13:32:02 -0500

    user managed identity?

--- cluster-services/providers.tf
+++ cluster-services/providers.tf
@@ -13,8 +13,9 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  # skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
+  # use_msi = true
 }
 
 data "azurerm_kubernetes_cluster" "cluster" {
