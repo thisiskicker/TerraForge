f1ec92b53c94dea775a972d8e49c8a736af9f4d4
thisiskicker <thisiskicker@gmail.com>
2023-09-01 12:51:04 -0500

    Push new provider code needs creds in TFC

--- cluster-services/providers.tf
+++ cluster-services/providers.tf
@@ -4,16 +4,34 @@ terraform {
       source = "gavinbunney/kubectl"
       version = "1.14.0"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
   }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+data "azurerm_kubernetes_cluster" "cluster" {
+  name                = "terraforge-cluster"
+  resource_group_name = "terraforge-jake"
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "${path.module}/kconfig"
+    host        = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
+    token       = data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key[0].client_key
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
   }
 }
 
 provider "kubectl" {
-  config_path = "${path.module}/kconfig"
-  # config_context = var.kcontext
+  host        = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
+  token       = data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key[0].client_key
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
 }
