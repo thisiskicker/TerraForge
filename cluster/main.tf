4a0a08184dbccb357d9342db460771e7af9cf15b
thisiskicker <thisiskicker@gmail.com>
2023-09-06 15:41:50 -0500

    back to original code
    create the resource groups

--- cluster/main.tf
+++ cluster/main.tf
@@ -1,15 +1,16 @@
-# resource "azurerm_resource_group" "rg" {
-#   name = var.resource_group_name
-#   location = var.location
-#   tags = {
-#     app = "terraforge"
-#   }
-# }
+resource "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+  location = var.location
+  tags = {
+    app = "terraforge"
+  }
+}
+
 resource "azurerm_kubernetes_cluster" "aks" {
   name = var.cluster_name
   kubernetes_version = var.kubernetes_version
   location = var.location
-  resource_group_name = var.resource_group_name #azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.rg.name
   dns_prefix = var.cluster_name
   node_resource_group = var.node_resource_group
   default_node_pool {
--- cluster/network.tf
+++ cluster/network.tf
@@ -1,8 +1,8 @@
 # Create Virtual Network
 resource "azurerm_virtual_network" "aksvnet" {
   name                = "aks-network"
-  location            = var.location #azurerm_resource_group.rg.location
-  resource_group_name = var.resource_group_name #azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/8"]
 }
 
@@ -10,6 +10,6 @@ resource "azurerm_virtual_network" "aksvnet" {
 resource "azurerm_subnet" "aks-default" {
   name                 = "aks-default-subnet"
   virtual_network_name = azurerm_virtual_network.aksvnet.name
-  resource_group_name  = var.resource_group_name #azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.rg.name
   address_prefixes     = ["10.240.0.0/16"]
 }
