4bc2a0789d07a68183fbe84d79bfb7805e4a1d0c
thisiskicker <thisiskicker@gmail.com>
2023-09-07 14:25:05 -0500

    update provider to work with azure

--- cluster-app/providers.tf
+++ cluster-app/providers.tf
@@ -4,12 +4,42 @@ terraform {
       source = "gavinbunney/kubectl"
       version = "1.14.0"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
   }
 }
 
-#TODO change config to get credentials from azure
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  # skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+  # use_msi = true
+}
+
+data "azurerm_kubernetes_cluster" "cluster" {
+  name                = "terraforge-cluster"
+  resource_group_name = "terraforge-jake"
+}
+
 provider "helm" {
   kubernetes {
-    config_content = terraform.workspace.var.kconfig
+    host                   = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
+    username               = data.azurerm_kubernetes_cluster.cluster.kube_config.0.username
+    password               = data.azurerm_kubernetes_cluster.cluster.kube_config.0.password
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
   }
+}
+
+provider "kubectl" {
+  load_config_file = false
+  host                   = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
+  username               = data.azurerm_kubernetes_cluster.cluster.kube_config.0.username
+  password               = data.azurerm_kubernetes_cluster.cluster.kube_config.0.password
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
 }
