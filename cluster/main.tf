66a47ca036c3cbcb5dcf2bf186153defc42616ad
thisiskicker <thisiskicker@gmail.com>
2023-09-11 12:26:04 -0500

    update vm type

--- cluster/main.tf
+++ cluster/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name = "system"
     node_count = var.system_node_count
-    vm_size = "standard_A2_v2"
+    vm_size = "standard_B2s"
     type = "VirtualMachineScaleSets"
     #availability_zones = [1, 2, 3]
     enable_auto_scaling = false
