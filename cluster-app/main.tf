e5723effba5f013e511ac19d8b71ee633a40ceaa
thisiskicker <thisiskicker@gmail.com>
2023-09-11 14:40:56 -0500

    remove

--- cluster-app/main.tf
+++ cluster-app/main.tf
@@ -1,30 +1,30 @@
-#use helm to install the terraforge app
-resource "helm_release" "terraforge-app" {
-  name       = "terraforge-app"
-  repository = "${path.module}/terraforge-app-chart"
-  chart      = "terraforge-app"
-  namespace = "terraforge"
-  create_namespace = true
-}
+# #use helm to install the terraforge app
+# resource "helm_release" "terraforge-app" {
+#   name       = "terraforge-app"
+#   repository = "${path.module}/terraforge-app-chart"
+#   chart      = "terraforge-app"
+#   namespace = "terraforge"
+#   create_namespace = true
+# }
 
-#create cluster issuer
-resource "kubectl_manifest" "cluster_issuer" {
-  yaml_body = file("${path.module}/cert-files/cluster-issuer.yml")
-}
+# #create cluster issuer
+# resource "kubectl_manifest" "cluster_issuer" {
+#   yaml_body = file("${path.module}/cert-files/cluster-issuer.yml")
+# }
 
-#create ssl cert
-resource "kubectl_manifest" "terraforge_cert" {
-  yaml_body = file("${path.module}/cert-files/certificate.yml")
-  namespace = helm_release.terraforge-app.namespace
-}
+# #create ssl cert
+# resource "kubectl_manifest" "terraforge_cert" {
+#   yaml_body = file("${path.module}/cert-files/certificate.yml")
+#   namespace = helm_release.terraforge-app.namespace
+# }
 
-#create image pull secret for custom images
-resource "kubernetes_secret" "regcred" {
-  metadata {
-    name = "regcred"
-  }
-  data = {
-    ".dockerconfigjson" = "var.REGCRED"
-  }
-  type = "kubernetes.io/dockerconfigjson"
-}
+# #create image pull secret for custom images
+# resource "kubernetes_secret" "regcred" {
+#   metadata {
+#     name = "regcred"
+#   }
+#   data = {
+#     ".dockerconfigjson" = "var.REGCRED"
+#   }
+#   type = "kubernetes.io/dockerconfigjson"
+# }
