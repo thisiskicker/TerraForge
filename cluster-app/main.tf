c37ad6d7c42ef366f63a1b62a2fe13b782273569
thisiskicker <thisiskicker@gmail.com>
2023-09-11 16:35:54 -0500

    renable

--- cluster-app/main.tf
+++ cluster-app/main.tf
@@ -1,42 +1,42 @@
-# resource "kubernetes_namespace" "terraforge" {
-#   metadata {
-#     annotations = {
-#       name = "terraforge"
-#       "linkerd.io/inject" = "enabled"
-#     }
-#     name = "terraforge"
-#   }
+resource "kubernetes_namespace" "terraforge" {
+  metadata {
+    annotations = {
+      name = "terraforge"
+      "linkerd.io/inject" = "enabled"
+    }
+    name = "terraforge"
+  }
+}
+
+#use helm to install the terraforge app
+resource "helm_release" "terraforge-app" {
+  name       = "terraforge-app"
+  repository = "${path.module}"
+  chart      = "terraforge-app-chart"
+  namespace = kubernetes_namespace.terraforge.metadata[0].name
+  #create_namespace = true
+}
+
+# #create cluster issuer
+# resource "kubectl_manifest" "cluster_issuer" {
+#   yaml_body = file("${path.module}/cert-files/cluster-issuer.yml")
 # }
 
-# #use helm to install the terraforge app
-# resource "helm_release" "terraforge-app" {
-#   name       = "terraforge-app"
-#   repository = "${path.module}"
-#   chart      = "terraforge-app-chart"
-#   namespace = kubernetes_namespace.terraforge.metadata[0].name
-#   #create_namespace = true
+# #create ssl cert
+# resource "kubectl_manifest" "terraforge_cert" {
+#   yaml_body = file("${path.module}/cert-files/certificate.yml")
+#   namespace = helm_release.terraforge-app.namespace
 # }
 
-# # #create cluster issuer
-# # resource "kubectl_manifest" "cluster_issuer" {
-# #   yaml_body = file("${path.module}/cert-files/cluster-issuer.yml")
-# # }
-
-# # #create ssl cert
-# # resource "kubectl_manifest" "terraforge_cert" {
-# #   yaml_body = file("${path.module}/cert-files/certificate.yml")
-# #   namespace = helm_release.terraforge-app.namespace
-# # }
-
-# #create image pull secret for custom images
-# #it just needs the config file unencoded
-# resource "kubernetes_secret" "regcred" {
-#   metadata {
-#     name = "regcred"
-#     namespace = kubernetes_namespace.terraforge.metadata[0].name
-#   }
-#   data = {
-#     ".dockerconfigjson" = var.REGCRED
-#   }
-#   type = "kubernetes.io/dockerconfigjson"
-# }
+#create image pull secret for custom images
+#it just needs the config file unencoded
+resource "kubernetes_secret" "regcred" {
+  metadata {
+    name = "regcred"
+    namespace = kubernetes_namespace.terraforge.metadata[0].name
+  }
+  data = {
+    ".dockerconfigjson" = var.REGCRED
+  }
+  type = "kubernetes.io/dockerconfigjson"
+}
