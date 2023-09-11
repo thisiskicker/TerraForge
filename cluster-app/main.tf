4cba4289cc989ae0e64eef5e78d90cc9f753f4b9
thisiskicker <thisiskicker@gmail.com>
2023-09-11 12:49:51 -0500

    reenable cluster services and app

--- cluster-app/main.tf
+++ cluster-app/main.tf
@@ -1,29 +1,29 @@
-# #use helm to install the terraforge app
-# resource "helm_release" "terraforge-app" {
-#   name       = "terraforge-app"
-#   repository = "../terraforge-app-chart"
-#   chart      = "terraforge-app"
-#   namespace = terraforge
-#   create_namespace = true
-# }
+#use helm to install the terraforge app
+resource "helm_release" "terraforge-app" {
+  name       = "terraforge-app"
+  repository = "../terraforge-app-chart"
+  chart      = "terraforge-app"
+  namespace = terraforge
+  create_namespace = true
+}
 
-# #create cluster issuer
-# resource "kubernetes_manifest" "cluster_issuer" {
-#   manifest = file("${path.module}/../cert-files/cluster-issuer.yaml")
-# }
+#create cluster issuer
+resource "kubernetes_manifest" "cluster_issuer" {
+  manifest = file("${path.module}/../cert-files/cluster-issuer.yaml")
+}
 
-# #create ssl cert
-# resource "kubernetes_manifest" "terraforge_cert" {
-#   manifest = file("${path.module}/../cert-files/certificate-issuer.yaml")
-# }
+#create ssl cert
+resource "kubernetes_manifest" "terraforge_cert" {
+  manifest = file("${path.module}/../cert-files/certificate-issuer.yaml")
+}
 
-# #create image pull secret for custom images
-# resource "kubernetes_secret" "regcred" {
-#   metadata {
-#     name = "regcred"
-#   }
-#   data = {
-#     ".dockerconfigjson" = "var.REGCRED"
-#   }
-#   type = "kubernetes.io/dockerconfigjson"
-# }
+#create image pull secret for custom images
+resource "kubernetes_secret" "regcred" {
+  metadata {
+    name = "regcred"
+  }
+  data = {
+    ".dockerconfigjson" = "var.REGCRED"
+  }
+  type = "kubernetes.io/dockerconfigjson"
+}
--- cluster-services/main.tf
+++ cluster-services/main.tf
@@ -1,63 +1,63 @@
-# #use helm to install linkerd crds
-# resource "helm_release" "linkerd-crds" {
-#   name       = "linkerd-crds"
-#   repository = "https://helm.linkerd.io/stable/"
-#   chart      = "linkerd-crds"
-#   create_namespace = true
-# }
+#use helm to install linkerd crds
+resource "helm_release" "linkerd-crds" {
+  name       = "linkerd-crds"
+  repository = "https://helm.linkerd.io/stable/"
+  chart      = "linkerd-crds"
+  create_namespace = true
+}
 
-# #use helm to install linkerd control plane
-# resource "helm_release" "linkerd-control-plane" {
-#   name       = "linkerd-control-plane"
-#   repository = "https://helm.linkerd.io/stable/"
-#   chart      = "linkerd-control-plane"
-#   namespace = helm_release.linkerd-crds.namespace
-#   set {
-#     name  = "identityTrustAnchorsPEM"
-#     value = tls_locally_signed_cert.issuer.ca_cert_pem
-#   }
-#   set {
-#     name  = "identity.issuer.tls.crtPEM"
-#     value = tls_locally_signed_cert.issuer.cert_pem
-#   }
-#   set {
-#     name  = "identity.issuer.tls.keyPEM"
-#     value = tls_private_key.issuer.private_key_pem
-#   }
-# }
+#use helm to install linkerd control plane
+resource "helm_release" "linkerd-control-plane" {
+  name       = "linkerd-control-plane"
+  repository = "https://helm.linkerd.io/stable/"
+  chart      = "linkerd-control-plane"
+  namespace = helm_release.linkerd-crds.namespace
+  set {
+    name  = "identityTrustAnchorsPEM"
+    value = tls_locally_signed_cert.issuer.ca_cert_pem
+  }
+  set {
+    name  = "identity.issuer.tls.crtPEM"
+    value = tls_locally_signed_cert.issuer.cert_pem
+  }
+  set {
+    name  = "identity.issuer.tls.keyPEM"
+    value = tls_private_key.issuer.private_key_pem
+  }
+}
 
-# #apply yaml for certmanager
-# #url = "https://github.com/cert-manager/cert-manager/releases/download/v1.12.0/cert-manager.yaml"
-# resource "kubectl_manifest" "certmanager_crd" {
-#   yaml_body = file("${path.module}/cert-manager.yaml")
-# }
+#apply yaml for certmanager
+#url = "https://github.com/cert-manager/cert-manager/releases/download/v1.12.0/cert-manager.yaml"
+resource "kubectl_manifest" "certmanager_crd" {
+  yaml_body = file("${path.module}/cert-manager.yaml")
+}
 
-# #use helm to install cert nginx
-# resource "helm_release" "ingress-nginx" {
-#   name       = "ingress-nginx"
-#   repository = "https://kubernetes.github.io/ingress-nginx"
-#   chart      = "ingress-nginx"
-#   namespace  = "ingress"
-#   create_namespace = true
-# }
+#use helm to install cert nginx
+resource "helm_release" "ingress-nginx" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace  = "ingress"
+  create_namespace = true
+}
 
-# #use helm to install kyverno
-# resource "helm_release" "kyverno" {
-#   name       = "kyverno"
-#   repository = "https://kyverno.github.io/kyverno/"
-#   chart      = "kyverno"
-#   namespace  = "kyverno"
-#   create_namespace = true
-# }
+#use helm to install kyverno
+resource "helm_release" "kyverno" {
+  name       = "kyverno"
+  repository = "https://kyverno.github.io/kyverno/"
+  chart      = "kyverno"
+  namespace  = "kyverno"
+  create_namespace = true
+}
 
-# #apply yaml to install kured 1.14.0
-# #url = "https://github.com/kubereboot/kured/releases/download/1.14.0/kured-1.14.0-dockerhub.yaml"
-# resource "kubectl_manifest" "kured" {
-#   yaml_body = file("${path.module}/kured-1.14.0-dockerhub.yaml")
-# }
+#apply yaml to install kured 1.14.0
+#url = "https://github.com/kubereboot/kured/releases/download/1.14.0/kured-1.14.0-dockerhub.yaml"
+resource "kubectl_manifest" "kured" {
+  yaml_body = file("${path.module}/kured-1.14.0-dockerhub.yaml")
+}
 
-# #apply yaml to install tekton
-# #url = "https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml"
-# resource "kubectl_manifest" "tekton" {
-#   yaml_body = file("${path.module}/tekton.yaml")
-# }
+#apply yaml to install tekton
+#url = "https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml"
+resource "kubectl_manifest" "tekton" {
+  yaml_body = file("${path.module}/tekton.yaml")
+}
