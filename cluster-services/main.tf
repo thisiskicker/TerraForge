d06fd85743673373d2560f0dd4f7d16976200a4a
thisiskicker <thisiskicker@gmail.com>
2023-09-13 12:48:24 -0500

    fix formatting

--- cluster-services/main.tf
+++ cluster-services/main.tf
@@ -46,8 +46,8 @@ resource "helm_release" "ingress-nginx" {
   namespace  = "ingress"
   create_namespace = true
   set {
-    name = "controller.service.annotations.service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"
-    value = "healthz"
+    name = "controller.service.annotations.\"service.beta.kubernetes.io/azure-load-balancer-health-probe-request-path\""
+    value = "/healthz"
   }
 }
 
